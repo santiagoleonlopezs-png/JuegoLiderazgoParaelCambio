c8305258a8085eec63830e916586e9824d3d9132
santiagoleonlopezs-png <santiagoleonlopezs@gmail.com>
2026-09-24 16:09:39 -0500

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-window.CHANGE_LAB_CONFIG = {
-  allowLocalFallback: true,
-  supabaseUrl: 'https://pcosytbiduzzvhiyfatx.supabase.co',
-  supabaseAnonKey: 'sb_publishable_dX1E2-tcy0WcPL0XxCmWOA_Ufuo55J1',
-  classCode: 'LID-CAMBIO'
-};
